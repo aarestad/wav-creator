25542a58912571d68bb013a22803d3795cf58858
Peter Aarestad <peter.aarestad@tempus.com>
2019-10-21 19:43:49 -0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,17 @@ use std::io;
 use std::io::Write;
 use std::i16;
 
+const CHUNK_LABEL: &[u8] = "RIFF".as_bytes();
+const FORMAT_LABEL: &[u8] = "WAVE".as_bytes();
+const DATA_LABEL: &[u8] = "data".as_bytes();
+const FMT_LABEL: &[u8] = "fmt ".as_bytes();
+
+const PCM_CHUNK_SIZE: u32 = 16u32; // for PCM
+
+const HEADER_SIZE: u32 = 8u32; // 4 bytes for id + 4 bytes for size
+
 fn main() -> io::Result<()> {
-    let data_str = "data".as_bytes();
 
-    // For each subchunk:
-    let header_size = 8u32; // 4 bytes for id + 4 bytes for size
-
-    // For the RIFF main header:
-    let chunk_id = "RIFF".as_bytes();
-    // Define ChunkSize at the bottom...
-    let format_id = "WAVE".as_bytes();
-
-    // For format subchunk:
-    let fmt_str = "fmt ".as_bytes();
-    let format_chunk_size = 16u32; // for PCM
     let format_type = 1u16; // Linear PCM
     let num_channels = 1u16; // Mono; 2 is stereo
     let sample_rate = 44_100u32;
@@ -30,7 +27,7 @@ fn main() -> io::Result<()> {
     let wave_size = 4;
     let samples = sample_rate * ms_duration / 1000;
     let data_chunk_size = samples * block_align as u32;
-    let file_size = wave_size + header_size + format_chunk_size + header_size + data_chunk_size;
+    let file_size = wave_size + HEADER_SIZE + PCM_CHUNK_SIZE + HEADER_SIZE + data_chunk_size;
 
     let mut wav_output_file = File::create("a_four_forty.wav")?;
 
@@ -38,9 +35,9 @@ fn main() -> io::Result<()> {
     // 0         4   ChunkID          Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
     // 4         4   ChunkSize        36 + SubChunk2Size, or more precisely: 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size) This is the size of the rest of the chunk following this number.  This is the size of the entire file in bytes minus 8 bytes for the two fields not included in this count: ChunkID and ChunkSize.
     // 8         4   Format           Contains the letters "WAVE" (0x57415645 big-endian form).
-    wav_output_file.write(chunk_id)?;
+    wav_output_file.write(CHUNK_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(file_size)?;
-    wav_output_file.write(format_id)?; // WAVE big-endian
+    wav_output_file.write(FORMAT_LABEL)?; // WAVE big-endian
 
     // "fmt " subchunk
     // 12        4   Subchunk1ID      Contains the letters "fmt " (0x666d7420 big-endian form).
@@ -51,8 +48,8 @@ fn main() -> io::Result<()> {
     // 28        4   ByteRate         == SampleRate * NumChannels * BitsPerSample/8
     // 32        2   BlockAlign       == NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels. I wonder what happens when this number isn't an integer?
     // 34        2   BitsPerSample    8 bits = 8, 16 bits = 16, etc.
-    wav_output_file.write(fmt_str)?;
-    wav_output_file.write_u32::<LittleEndian>(format_chunk_size)?;
+    wav_output_file.write(FMT_LABEL)?;
+    wav_output_file.write_u32::<LittleEndian>(PCM_CHUNK_SIZE)?;
     wav_output_file.write_u16::<LittleEndian>(format_type)?;
     wav_output_file.write_u16::<LittleEndian>(num_channels)?;
     wav_output_file.write_u32::<LittleEndian>(sample_rate)?;
@@ -64,7 +61,7 @@ fn main() -> io::Result<()> {
     // 36        4   Subchunk2ID      Contains the letters "data" (0x64617461 big-endian form).
     // 40        4   Subchunk2Size    == NumSamples * NumChannels * BitsPerSample/8 This is the number of bytes in the data. You can also think of this as the size of the read of the subchunk following this number.
     // 44        *   Data             The actual sound data.
-    wav_output_file.write(data_str)?;
+    wav_output_file.write(DATA_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(data_chunk_size)?;
 
     let mut signal = signal::rate(sample_rate as f64)
