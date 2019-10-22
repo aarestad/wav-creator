34ef27546482a34ddf188ffe9d0587a307789cf8
Peter Aarestad <peter.aarestad@tempus.com>
2019-10-22 11:51:35 -0500

    documentation etc.

--- src/main.rs
+++ src/main.rs
@@ -22,68 +22,84 @@ const FORMAT_TYPE: u16 = 1u16;
 const SAMPLE_RATE: u32 = 44_100u32;
 /// Standard 16-bit sound resolution
 const BITS_PER_SAMPLE: u16 = 16u16;
+/// Mono (for now
+const NUM_CHANNELS: u16 = 1u16;
 
 fn main() -> io::Result<()> {
-    write(
-        1,
+    write_wav(
         1000,
         440.0,
-        i16::MAX.into(),
+        i16::MAX,
         Path::new("a_four_forty.wav"),
     )
 }
 
-fn write(
-    num_channels: u16,
+///
+/// Write a WAV file to `file_name`
+///
+/// WAV file format:
+///
+/// Offset      Num bytes   Field ID        Description
+/// (SubChunk 0)
+/// 0           4           ChunkID         "RIFF" in ASCII
+/// 4           4           ChunkSize       36 + SubChunk2Size, or more precisely:
+///                                         4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
+///                                         This is the size of the rest of the file following this
+///                                         number
+/// 8           4           Format          "WAVE" in ASCII
+/// (SubChunk 1)
+/// 12          4           Subchunk1ID     "fmt " in ASCII
+/// 16          4           Subchunk1Size   16 (for PCM) - the size of the rest of this subchunk
+/// 20          2           AudioFormat     1 (for PCM)
+/// 22          2           NumChannels     Mono = 1, Stereo = 2, etc.
+/// 24          4           SampleRate      Usually 44100 - samples per second
+/// 28          4           ByteRate        Bytes per second - sample rate times bytes per sample
+/// 32          2           BlockAlign      Bytes per frame (BitsPerSample/8 * NumChannels)
+/// 34          2           BitsPerSample   Usually 16
+/// (SubChunk 2)
+/// 36          4           Subchunk2ID      "data" in ASCII
+/// 40          4           Subchunk2Size    Number of samples (i.e. SampleRate * length of the file
+///                                          in seconds) * BlockAlign
+/// 44          *           Data             The actual sound data.
+///
+/// This function always writes the WAV with the following parameters:
+/// Sample rate: 44,100 samples/sec
+/// Bits per sample: 16
+/// Channels: 1 (monoaural)
+/// The sound produced is a `ms_duration` millisecond--long sine wave at `freq` Hz, scaled to `amp` amplitude
+fn write_wav(
     ms_duration: u32,
     freq: f64,
-    amp: f64,
+    amp: i16,
     file_name: &Path,
 ) -> io::Result<()> {
-    let bytes_per_sample: u16 = (num_channels * BITS_PER_SAMPLE) / 8;
+    let bytes_per_sample: u16 = (NUM_CHANNELS * BITS_PER_SAMPLE) / 8;
     let num_samples: u32 = SAMPLE_RATE * ms_duration / 1000;
     let data_chunk_size: u32 = num_samples * (bytes_per_sample as u32);
     let file_size: u32 = 4 + HEADER_SIZE + FMT_CHUNK_SIZE + HEADER_SIZE + data_chunk_size;
 
     let mut wav_output_file = File::create(file_name)?;
 
-    // HEADER
-    // 0         4   ChunkID          Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
-    // 4         4   ChunkSize        36 + SubChunk2Size, or more precisely: 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size) This is the size of the rest of the chunk following this number.  This is the size of the entire file in bytes minus 8 bytes for the two fields not included in this count: ChunkID and ChunkSize.
-    // 8         4   Format           Contains the letters "WAVE" (0x57415645 big-endian form).
     wav_output_file.write(RIFF_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(file_size)?;
     wav_output_file.write(FORMAT_LABEL)?; // WAVE big-endian
 
-    // "fmt " subchunk
-    // 12        4   Subchunk1ID      Contains the letters "fmt " (0x666d7420 big-endian form).
-    // 16        4   Subchunk1Size    16 for PCM.  This is the size of the rest of the Subchunk which follows this number.
-    // 20        2   AudioFormat      PCM = 1 (i.e. Linear quantization) Values other than 1 indicate some form of compression.
-    // 22        2   NumChannels      Mono = 1, Stereo = 2, etc.
-    // 24        4   SampleRate       8000, 44100, etc.
-    // 28        4   ByteRate         == SampleRate * NumChannels * BitsPerSample/8
-    // 32        2   BlockAlign       == NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels. I wonder what happens when this number isn't an integer?
-    // 34        2   BitsPerSample    8 bits = 8, 16 bits = 16, etc.
     wav_output_file.write(FMT_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(FMT_CHUNK_SIZE)?;
     wav_output_file.write_u16::<LittleEndian>(FORMAT_TYPE)?;
-    wav_output_file.write_u16::<LittleEndian>(num_channels)?;
+    wav_output_file.write_u16::<LittleEndian>(NUM_CHANNELS)?;
     wav_output_file.write_u32::<LittleEndian>(SAMPLE_RATE)?;
     wav_output_file.write_u32::<LittleEndian>(SAMPLE_RATE * (bytes_per_sample as u32))?;
     wav_output_file.write_u16::<LittleEndian>(bytes_per_sample)?;
     wav_output_file.write_u16::<LittleEndian>(BITS_PER_SAMPLE)?;
 
-    // "data" subchunk
-    // 36        4   Subchunk2ID      Contains the letters "data" (0x64617461 big-endian form).
-    // 40        4   Subchunk2Size    == NumSamples * NumChannels * BitsPerSample/8 This is the number of bytes in the data. You can also think of this as the size of the read of the subchunk following this number.
-    // 44        *   Data             The actual sound data.
     wav_output_file.write(DATA_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(data_chunk_size)?;
 
     let mut signal = signal::rate(SAMPLE_RATE.into())
         .const_hz(freq)
         .sine()
-        .scale_amp(amp);
+        .scale_amp(amp.into());
 
     for _ in 0..num_samples {
         let sample = signal.next()[0];
