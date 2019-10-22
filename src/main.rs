096b2b5aa2094f79ab302fc3f6c863a527bab72d
Peter Aarestad <peter.aarestad@tempus.com>
2019-10-22 11:27:11 -0500

    refactoring and notes

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
 use byteorder::{LittleEndian, WriteBytesExt};
 use sample::{signal, Signal};
 use std::fs::File;
+use std::i16;
 use std::io;
 use std::io::Write;
-use std::i16;
+use std::path::Path;
 
-const CHUNK_LABEL: &[u8] = b"RIFF";
+/// Chunk labels
+const RIFF_LABEL: &[u8] = b"RIFF";
 const FORMAT_LABEL: &[u8] = b"WAVE";
 const DATA_LABEL: &[u8] = b"data";
 const FMT_LABEL: &[u8] = b"fmt ";
 
-const PCM_CHUNK_SIZE: u32 = 16u32; // for PCM
-const HEADER_SIZE: u32 = 8u32; // 4 bytes for id + 4 bytes for size
-const FORMAT_TYPE: u16 = 1u16; // Linear PCM
+/// metadata chunk is always 16 bytes
+const FMT_CHUNK_SIZE: u32 = 16u32;
+/// 8 bytes for 4-byte string label plus 4-byte subchunk size
+const HEADER_SIZE: u32 = 8u32;
+/// 1 means PCM
+const FORMAT_TYPE: u16 = 1u16;
+/// Standard sample rate: 44.1 KHz
 const SAMPLE_RATE: u32 = 44_100u32;
+/// Standard 16-bit sound resolution
 const BITS_PER_SAMPLE: u16 = 16u16;
 
 fn main() -> io::Result<()> {
-    write(1, 1000, 440.0, i16::MAX.into())
+    write(
+        1,
+        1000,
+        440.0,
+        i16::MAX.into(),
+        Path::new("a_four_forty.wav"),
+    )
 }
 
-fn write(num_channels: u16, ms_duration: u32, freq: f64, amp: f64) -> io::Result<()> {
-    let bytes_per_sample = (num_channels * BITS_PER_SAMPLE) / 8;
-    let num_samples = SAMPLE_RATE * ms_duration / 1000;
-    let data_chunk_size = num_samples * (bytes_per_sample as u32);
-    let file_size = 4 + HEADER_SIZE + PCM_CHUNK_SIZE + HEADER_SIZE + data_chunk_size;
+fn write(
+    num_channels: u16,
+    ms_duration: u32,
+    freq: f64,
+    amp: f64,
+    file_name: &Path,
+) -> io::Result<()> {
+    let bytes_per_sample: u16 = (num_channels * BITS_PER_SAMPLE) / 8;
+    let num_samples: u32 = SAMPLE_RATE * ms_duration / 1000;
+    let data_chunk_size: u32 = num_samples * (bytes_per_sample as u32);
+    let file_size: u32 = 4 + HEADER_SIZE + FMT_CHUNK_SIZE + HEADER_SIZE + data_chunk_size;
 
-    let mut wav_output_file = File::create("a_four_forty.wav")?;
+    let mut wav_output_file = File::create(file_name)?;
 
     // HEADER
     // 0         4   ChunkID          Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
     // 4         4   ChunkSize        36 + SubChunk2Size, or more precisely: 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size) This is the size of the rest of the chunk following this number.  This is the size of the entire file in bytes minus 8 bytes for the two fields not included in this count: ChunkID and ChunkSize.
     // 8         4   Format           Contains the letters "WAVE" (0x57415645 big-endian form).
-    wav_output_file.write(CHUNK_LABEL)?;
+    wav_output_file.write(RIFF_LABEL)?;
     wav_output_file.write_u32::<LittleEndian>(file_size)?;
     wav_output_file.write(FORMAT_LABEL)?; // WAVE big-endian
 
@@ -46,7 +65,7 @@ fn write(num_channels: u16, ms_duration: u32, freq: f64, amp: f64) -> io::Result
     // 32        2   BlockAlign       == NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels. I wonder what happens when this number isn't an integer?
     // 34        2   BitsPerSample    8 bits = 8, 16 bits = 16, etc.
     wav_output_file.write(FMT_LABEL)?;
-    wav_output_file.write_u32::<LittleEndian>(PCM_CHUNK_SIZE)?;
+    wav_output_file.write_u32::<LittleEndian>(FMT_CHUNK_SIZE)?;
     wav_output_file.write_u16::<LittleEndian>(FORMAT_TYPE)?;
     wav_output_file.write_u16::<LittleEndian>(num_channels)?;
     wav_output_file.write_u32::<LittleEndian>(SAMPLE_RATE)?;
@@ -66,7 +85,7 @@ fn write(num_channels: u16, ms_duration: u32, freq: f64, amp: f64) -> io::Result
         .sine()
         .scale_amp(amp);
 
-    for _ in 0 .. num_samples {
+    for _ in 0..num_samples {
         let sample = signal.next()[0];
 
         println!("{:?}", sample);
